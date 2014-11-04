b736c8bdb8eb467c222e2d08a762cb2be55ceecd
Drew <drew.r.wilson@gmail.com>
2014-11-03 22:28:53 -0800

    cut out local storage stuff

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ This script loads data from a google doc about VegFests across the country and p
 
 Here's the plan:
 * load CSV from gdocs
-* check if all neccesary fields are there (location, 2014 date, name, website)
 * make a geojson (2014 dates)
-* write out geojson to a local directory
-* seperately, run sync.js periodically to sync data directory with s3
+* upload the geojson to s3 in two locations:
+** one as vegfest.geojson
+** one as timestampe-vegfests.geojson so that we can roll back if we need to.
 
 */
 
@@ -20,13 +20,14 @@ var request = require('request'),
     knox = require('knox');
 
 var config = {
-            "outputDirectory" : 'data/',
             "csvLink" : "https://docs.google.com/spreadsheet/ccc?key=1Wna5S_59sy1ycidSsFERKefd_Wy7oajyCknifkpzdcU&output=csv",
             "filename" : "vegfests",
             "accessKeyID" : process.env.ACCESS_KEY,
             "secretKey" : process.env.SECRET_KEY,
             "bucket" : process.env.BUCKET,
-            "remoteDirectory" : process.env.REMOTE_DIRECTORY
+            "remoteDirectory" : process.env.REMOTE_DIRECTORY,
+            "latField" : "2014 Latitude",
+            "lonField" : "2014 Longitude"
           }
 
 var client = knox.createClient({
@@ -34,39 +35,6 @@ var client = knox.createClient({
   , secret: config.secretKey
   , bucket: config.bucket
 });
-
-function ensureExists(path, mask, cb) {
-    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
-        cb = mask;
-        mask = 0777;
-    }
-    fs.mkdir(path, mask, function(err) {
-        if (err) {
-            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
-            else cb(err); // something else went wrong
-        } else cb(null); // successfully created folder
-    });
-}
-
-function initialize (directory, callback) {
-  //make sure data directory exists, if not create it
-  ensureExists(__dirname + '/' + directory, 0744, function(err) {
-    if (err) {
-      console.log(err);
-    } // handle folder creation error
-    else {
-      callback();
-    }// we're all good
-  });
-
-}
-
-function writeToFile (data, filename, callback) {
-  fs.writeFile(filename, data, function (err) {
-    if (err) return console.log(err);
-    callback();
-  });
-}
 
 function writeToS3 (geoJSON, filename, callback) {
   var req = client.put(filename, {
@@ -83,28 +51,19 @@ function writeToS3 (geoJSON, filename, callback) {
 
 }
 
-initialize (config.outputDirectory, function () {
-    console.log('Using this directory for data output: ' + config.outputDirectory);
-});
-
 request(config.csvLink, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     // console.log(body) // Print the CSV
     console.log('Downloaded CSV of google doc successfully');
 
     var geoJson = csv2geojson.csv2geojson(body, {
-      latfield: '2014 Latitude',
-      lonfield: '2014 Longitude',
+      latfield: config.latField,
+      lonfield: config.lonField,
       delimiter: ','
     },
     function(err, data) {
-        // err has any parsing errors
-        // data is the data.
         jsonString = JSON.stringify(data);
         var timestamp = Date.now();
-        writeToFile(jsonString, config.outputDirectory + timestamp + '-' + config.filename + '.geojson', function () {
-            console.log('Successfully wrote data to geojson: ' + timestamp + '-' + config.filename + '.geojson');
-          })
         writeToS3(jsonString, config.remoteDirectory + timestamp + '-' + config.filename + '.geojson', function () {
             // console.log('Successfully wrote data to S3.);
           });
